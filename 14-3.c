8c170fac0db640d3d8942c569eac6bdb7cf837df
X2Borgo <75789972+X2Borgo@users.noreply.github.com>
2024-03-14 08:56:50 +0100

    Update 14-3.c

--- 14-3.c
+++ 14-3.c
@@ -1,1 +1,12 @@
 //14-3
+
+malloc / free -> new / delete
+
+lista di inizializzazione costruttore:
+	public:
+		Stack(int s): TOS(0), size(s), buffer(new data[s])
+
+	protected:
+		int TOS;
+		int size;
+		data *buffer;
